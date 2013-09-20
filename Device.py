1d77d9ccea28af89633280899507154fa955e957
Alex <aspistrel@gmail.com>
2013-09-20 16:01:21 +0400

    test

--- Device.py
+++
@@ -1,9 +0,0 @@
-class Device():
-    """ Instance that serves items(requests)"""
-    def __init__(self):
-        self.state = False
-        self.free_time = 0
-        self.end_time = 0
-
-    def __repr__(self):
-            return repr((self.state, self.free_time, self.end_time))
--- Item.py
+++
@@ -1,8 +0,0 @@
-class Item():
-    """ Instance that serves by device """
-    def __init__(self, prc_time):
-        self.processing_time = prc_time
-        self.enter_stack_time = 0
-
-    def __repr__(self):
-                return repr((self.processing_time, self.enter_stack_time))
